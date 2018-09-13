07da2fcd3f6a03bfbfdbb6db3d97b341962663d3
Javier Borquez <hi@javier.xyz>
2018-09-13 17:13:35 -0500

    Update demo

---
+++ app.js
@@ -0,0 +1,64 @@
+const iframeEl = document.querySelector(".iframeContainer");
+const debugEl = document.querySelector(".debug");
+
+let motionBeta = 0;
+let rotation = 0;
+let accelerationX = 0;
+let accelerationY = 0;
+
+let style = 0;
+
+let last = new Date().getTime();
+
+function handleDeviceOrientationEvent(event) {
+  motionBeta = -1 * event.beta;
+}
+
+function handleDeviceMotionEvent(event) {
+  const rotationRate = event.rotationRate;
+  const acceleration = event.acceleration;
+
+  rotation = rotationRate.gamma;
+  accelerationX = (3 * accelerationX + Math.pow(acceleration.y, 1) * -10) / 4;
+  accelerationY = (3 * accelerationY + Math.pow(acceleration.x, 1) * -10) / 4;
+}
+
+function paint() {
+  /*
+  const time = new Date().getTime();
+  debugEl.innerHTML = parseInt(1000 / (time - last));
+  last = time;
+  */
+
+  if (style == 0) iframeEl.style.transform = `rotate(${motionBeta}deg)`;
+
+  if (style == 1)
+    iframeEl.style.transform = `rotate(${rotation *
+      0.06}deg) translatex(${accelerationX}px) translatey(${accelerationY}px)`;
+
+  if (style == 2)
+    iframeEl.style.transform = `rotate(${rotation * 0.06 + motionBeta}deg)`;
+
+  if (style == 3)
+    iframeEl.style.transform = `rotate(${rotation * 0.04 +
+      motionBeta /
+        2}deg)  translatex(${accelerationX}px) translatey(${accelerationY}px)`;
+
+  window.requestAnimationFrame(paint);
+}
+paint();
+
+// Add event listeners to all the buttons to change the algorithm
+[...document.querySelectorAll("[data-value]")].forEach(el => {
+  el.addEventListener("click", () => {
+    document
+      .querySelector(".option-element.-active")
+      .classList.remove("-active");
+    el.classList.add("-active");
+
+    style = el.dataset.value;
+  });
+});
+
+window.addEventListener("deviceorientation", handleDeviceOrientationEvent, true);
+window.addEventListener("devicemotion", handleDeviceMotionEvent, true);
